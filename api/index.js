e0010bf00900d684c55932c3507b7be8b08f3bda
JulianBrg <julian.barragan@alumno.uttehuacan.edu.mx>
2024-04-16 13:23:13 -0600

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 //leer el archvivo .env
 dotenv.config();
@@ -18,9 +19,14 @@ mongoose
 
 // Inicializar el servidor
 const app = express();
+
+// Middleware
+app.use(express.json());
+
 // Configuracion del servidor
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
